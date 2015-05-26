8ad47bfa031230b6da85bd23e7d5871646e1dfd0
AndyHT <843018739@qq.com>
2015-05-26 23:25:02 +0800

    add hibernate

---
+++ src/main/java/com/huoteng/hibernate/TargetUrlEntity.java
@@ -0,0 +1,53 @@
+package com.huoteng.hibernate;
+
+import javax.persistence.*;
+
+/**
+ * Created by huoteng on 5/26/15.
+ */
+@Entity
+@Table(name = "target-url", schema = "", catalog = "search")
+public class TargetUrlEntity {
+    private int id;
+    private String url;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "url")
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TargetUrlEntity that = (TargetUrlEntity) o;
+
+        if (id != that.id) return false;
+        if (url != null ? !url.equals(that.url) : that.url != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (url != null ? url.hashCode() : 0);
+        return result;
+    }
+}
